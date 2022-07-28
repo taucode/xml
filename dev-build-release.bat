2b8e30837ac810a9ec761cec09d9d0ac065b6dab
Andrey Kovalenko <ak@taucode.com>
2022-07-28 20:02:54 +0300

    Fixed nuspec.

--- dev-build-release.bat
+++ dev-build-release.bat
@@ -6,4 +6,4 @@ dotnet build --configuration Release
 dotnet test -c Debug .\test\TauCode.Xml.Tests\TauCode.Xml.Tests.csproj
 dotnet test -c Release .\test\TauCode.Xml.Tests\TauCode.Xml.Tests.csproj
 
-nuget pack nuget\TauCode.Db.nuspec
+nuget pack nuget\TauCode.Xml.nuspec
